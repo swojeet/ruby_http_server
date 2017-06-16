b1cc6a4bc77294eb0e4737688373d6bd42d73ce3
swojeet <swojeetkayastha@gmail.com>
2017-06-16 14:35:45 +0545

    Implement HTTP Time Server

--- ruby_http_time_server.rb
+++ ruby_http_time_server.rb
@@ -3,58 +3,30 @@ require 'thread'
 require 'socket'
 
 server = TCPServer.new 5678
-POOL_SIZE = 10
+POOL_SIZE = 1000
 
 jobs = Queue.new
 
-# 10_000.times {|i| jobs.push i}
-
-# puts Benchmark.measure{
-#   while session=server.accept
-#     workers = (POOL_SIZE).times.map do
-#       Thread.new do
-#         begin
-#           while x = session.pop(true)
-#             session.puts "Time is #{Time.now.to_f * 1000} #{count}"
-#           end
-#         rescue
-#           ThreadError
-#         end
-#       end
-#     end
-#     workers.map(&:join)
-#     session.close
-#   end
-# }
-
 workers = (POOL_SIZE).times.map do
-  begin
-    while session = server.accept
-      jobs.push(session)
-      puts jobs.inspect
-      # p jobs
-      # request = session.gets
-      # puts request
+  Thread.new do
+    begin
+      while session = server.accept
+        jobs << session
+        # request = session.gets
+        # puts request
 
-      Thread.new do
         while x = jobs.pop(true)
           session.print "HTTP/1.1 200\r\n"
           session.print "Content-Type: text/html\r\n"
           session.print "\r\n"
-          session.puts "Eh yo! Time is #{Time.now}"
+          session.puts "Hey there! Time is #{Time.now}"
           session.close
         end
       end
+    rescue ThreadError
     end
-  rescue
-    ThreadError
   end
 end
+# workers.abort_on_exception=false
 
 workers.map(&:join)
-  # session.print "HTTP/1.1 200\r\n"
-  # session.print "Content-Type: text/html\r\n"
-  # session.print "\r\n"
-  # session.puts "Eh yo! Time is #{Time.now}"
-  #
-  # session.close
